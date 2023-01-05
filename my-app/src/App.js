78b544ae44a2edfd0a256ebb48ac15ee25f5ae08
João Victor Leoni <joaovictorleoni2021@gmail.com>
2023-01-04 23:28:48 -0300

    .

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola React, meu Nome é joão Victor</h1>
+      <p>Meu primeiro App em ReactJS</p>
+    </div>
+  );
+}
+
+export default App;
