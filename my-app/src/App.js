fd0d49b691c5b58715fbd1298f1984e2fbb7d00f
João Victor Leoni <joaovictorleoni2021@gmail.com>
2023-01-05 21:47:38 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'João'
+  const newName = name.toUpperCase()
+
+  function sum(a,b){
+    return a + b
+  }
+
+  const url  ='https://via.placeholder.com/150'
   return (
     <div className="App">
       <h1>Ola React, meu Nome é joão Victor</h1>
       <p>Meu primeiro App em ReactJS</p>
+      <p>olá, {newName}</p>
+      <p>soma: {sum(5,5)}</p>
+      <img src={url} alt="Minha Imagem"/>
     </div>
   );
 }
